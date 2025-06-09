05923e24e77e39e7274d8e4dd4de089a4b0defd2
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-06-09 15:28:20 +0000

    Unit5-01 HTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+
+js/script.js
+// Copyright (c) 2022 Ms Raffin All rights reserved
+// Created by: Ms Raffin
+// Created on: May 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+* This function displays all numbers from up to the user number */
+function numberDisplay() {
+    // initialize the counter to 0
+    let counter = 0
+    // initalize numbers as an empty string
+    let numbers = ""
+    let numbers2 = ""
+    // get the user number
+    let userNumMin = parseInt(document.getElementById('minNumber').value)
+    let userNumMax = parseInt(document.getElementById('maxNumber').value)
+
+    while (counter <= userNumMin) {
+        // build the string of numbers with a line break each time
+        numbers = numbers + counter + "<br>"
+        // increment the counter
+        counter = counter +1
+    }
+    while (counter <= userNumMax) {
+        // build the string of numbers with a line break each time
+        numbers2 = numbers2 + counter + "<br>"
+        // increment the counter
+        counter = counter +1
+    }
+
+    // return the string of numbers back to html
+    document.getElementById('min').innerHTML = numbers
+    document.getElementById('max').innerHTML = numbers2
+}
